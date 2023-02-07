0b926714384ae505a46b83772a64e9e061a51a9c
Alfredo Correa <alfredo.correa@gmail.com>
2023-02-07 15:16:30 -0800

    use inq's fabs to avoid confusion with abs/std::abs

--- src/eigensolvers/steepest_descent.hpp
+++ src/eigensolvers/steepest_descent.hpp
@@ -78,10 +78,7 @@ void steepest_descent(const operator_type & ham, const preconditioner_type & pre
 
 							 auto den = cb + sqrt(cb*cb - 4.0*ca*cc);
 
-							using std::abs;
-							static_assert(not std::is_same_v<decltype(abs(den)), int   >, "wrong abs called");
-							static_assert(    std::is_same_v<decltype(abs(den)), double>, "wrong abs called");
-							if(abs(den) < 1e-15) { //this happens if we are perfectly converged
+							if(inq::fabs(den) < 1e-15) { //this happens if we are perfectly converged
 								lam[ist] = complex{0.0, 0.0};
 							} else {
 								lam[ist] = complex{2.0*cc/den, 0.0};
