deb60654e8097720f02a711a4dc4aaf61672c6c4
Rafi Ullah <rraffiu@gmail.com>
2023-12-06 22:24:45 -0600

    Align right

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -80,6 +80,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_style = 'inq.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
