4d7fde4392580286672c86cd50297183dea994d9
jbost7397 <johnlucasbost@gmail.com>
2024-10-22 12:56:47 -0400

    Incorporating Chris' updates to jade, modifying the structure of basis_mapping (WIP)

--- src/wannier/basis_mapping.hpp
+++ src/wannier/basis_mapping.hpp
@@ -22,13 +22,56 @@
 namespace inq {
 namespace wannier {
 
+class basis_mapping {
+private:
+  inq::parallel::communicator comm_; 
+  long nprocs_;         
+  long myproc_;        
+  long np0_;           
+  long np1_;           
+  long np2_;           
+  std::vector<long> np2_loc_; 
+  std::vector<long> np2_first_;
+  long np012loc_;       
+
+
+public:
+  basis_mapping(const inq::basis::real_space& basis) : comm_(basis.comm()) { 
+    nprocs_ = comm_.size();
+    myproc_ = comm_.rank();
+    np0_ = basis.sizes()[0];
+    np1_ = basis.sizes()[1];
+    np2_ = basis.sizes()[2];
+
+
+    // Initialize np2_loc_ and np2_first_ using block-cyclic distribution function
+    np2_loc_.resize(nprocs_);
+    np2_first_.resize(nprocs_);
+    for (int iproc = 0; iproc < nprocs_; ++iproc) {
+      np2_loc_[iproc] = partition.local_size(iproc);
+      np2_first_[iproc] = partition.start(iproc);
+    }
+
+    np012loc_ = np0_ * np1_ * np2_loc_[myproc_];
+    }
+
+    long np0() const { return np0_; }
+    long np1() const { return np1_; }
+    long np2() const { return np2_; }
+    long np2_loc() const { return np2_loc_[myproc_]; }
+    long np2_first() const { return np2_first_[myproc_]; }
+    long np2_loc(int nproc) const { return np2_loc_[nproc]; }
+    long np2_first(int nproc) const { return np2_first_[nproc]; }
+    auto comm() const { return comm_; }
+};
+
 class block_cyclic_partition : public parallel::arbitrary_partition {
 
 public:
 	template <typename CommType>
 	block_cyclic_partition(long size, CommType comm, int block_size) :
-        parallel::arbitrary_partition(size, comm) {
-
+        parallel::arbitrary_partition(size, comm) {  
+	
 		auto comm_size = comm.size();
 		auto comm_rank = comm.rank();
         	int num_blocks = (size + block_size - 1) / block_size;
@@ -51,38 +94,67 @@ public:
 };
 
 std::vector<int> generate_ipack(const block_cyclic_partition& partition, int nvec, int np2) {
-    std::vector<int> ipack;
-    ipack.reserve(nvec * np2);
+	std::vector<int> ipack;
+	ipack.reserve(nvec * np2); 
 
-    long global_index = 0;
-    for (int iproc = 0; iproc < partition.size(); ++iproc) {
-        for (int ivec = 0; ivec < nvec; ++ivec) {
-            for (long i = partition.start(iproc); i < partition.end(iproc); ++i) {
-                ipack.push_back(global_index);
-                global_index++;
-            }
-        }
-    }
-    return ipack;
+	long global_index = 0;
+	for (int iproc = 0; iproc < partition.comm_size(); ++iproc) {
+		int isource = np2_first_[iproc];
+		int sz = np2_loc_[iproc];
+		for (int ivec = 0; ivec < nvec; ++ivec) {
+			for ( int i = 0; i < sz; i++ )
+			//for (long i = partition.start(iproc); i < partition.end(iproc); ++i) {
+				ipack_[isource+i] = idest + i;
+                		//ipack.push_back(global_index);
+		 		//global_index++;
+			}
+		idest += sz;
+		isource += np2_;
+        	}
+	}
+	return ipack;
 }
 
 /*
-//need to recover functionality from Qb@ll's 'rod' structure... need to think about equivalent functionality in Inq
-//implementation for when the basis is real
-std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int np0, int np1, int np2, int nvec) {
+ *Mimicking Qb@ll's Rod structure:
+long global_index = np2_first_[iproc] + ipoint; //Global index of the grid point
+
+auto coords = basis_.point_op().from_symmetric_range(basis_.to_symmetric_range(global_index));
+
+int h = coords[0]; 
+int k = coords[1]; 
+
+basis_.nrods() = basis_.size(); //??? or maybe basis_.sizes(0) * basis_.sizes(1);
+basis_.nrod_loc(n) = bm.np2_loc_(n); //???
+ */
+//std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int np0, int np1, int np2, int nvec) {
+std::vector<int> generate_iunpack(const block_cyclic_partition& partition, basis_mapping bm) {
+    auto np0 = bm.np0();
+    auto np1 = bm.np1();
+    auto np2 = bm.np2();
+    auto np2_loc = bm.np2_loc();
+    auto comm = bm.comm();
     std::vector<int> iunpack;
-    iunpack.reserve((2 * basis_.nrods() -1) * partition.local_size());
+    //iunpack.reserve((2 * basis_.nrods() - 1) * partition.local_size());
+    iunpack.reserve((2 * basis_.size() - 1) * partition.local_size());
 
     for (int l = 0; l < partition.local_size(); ++l) {
-        iunpack.push_back(l * np0 * np1);
+        iunpack.push_back(l * np0 * np1); 
     }
     int isource_p = partition.local_size();
     int isource_m = 2 * partition.local_size();
 
 
-    for (int irod = 1; irod < basis_.nrod_loc(0); irod++) {
-        int hp = basis_.rod_h(0, irod);
-        int kp = basis_.rod_k(0, irod);
+    //for (int irod = 1; irod < basis_.nrod_loc(0); irod++) {
+    for (int irod = 1; irod < np2_loc; irod++) {
+        //int hp = basis_.rod_h(0, irod);
+        //int kp = basis_.rod_k(0, irod);
+	long global_index = bm.np2_first() + irod; 
+
+        auto coords = basis_.point_op().from_symmetric_range(basis_.to_symmetric_range(global_index));
+
+        int hp = coords[0]; 
+        int kp = coords[1]; 
         if (hp < 0) hp += np0;
         if (kp < 0) kp += np1;
 
@@ -103,10 +175,18 @@ std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int n
         }
     }
 
-    for (int iproc = 1; iproc < comm_.size(); iproc++) {
-        for (int irod = 0; irod < basis_.nrod_loc(iproc); irod++) {
-            int hp = basis_.rod_h(iproc, irod);
-            int kp = basis_.rod_k(iproc, irod);
+    for (int iproc = 1; iproc < comm.size(); iproc++) {
+        //for (int irod = 0; irod < basis_.nrod_loc(iproc); irod++) {
+        for (int irod = 0; irod < bm.np2_loc(iproc); irod++) {
+            //int hp = basis_.rod_h(iproc, irod);
+            //int kp = basis_.rod_k(iproc, irod);
+	    long global_index = bm.np2_first(iproc) + irod; 
+
+            auto coords = basis_.point_op().from_symmetric_range(basis_.to_symmetric_range(global_index));
+
+       	    int hp = coords[0]; 
+      	    int kp = coords[1]; 
+
             if (hp < 0) hp += np0;
             if (kp < 0) kp += np1;
 
@@ -130,17 +210,31 @@ std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int n
     }
     return iunpack;
 }
-
+/*
 //case when the basis is complex
-std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int np0, int np1, int np2, int nvec) {
+//std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int np0, int np1, int np2, int nvec) {
+std::vector<int> generate_iunpack(const block_cyclic_partition& partition, basis_mapping bm) {
     std::vector<int> iunpack;
-    iunpack.reserve(basis_.nrods() * partition.local_size());
+    auto np0 = bm.np0();
+    auto np1 = bm.np1();
+    auto np2 = bm.np2();
+    auto np2_loc = bm.np2_loc();
+    auto comm = bm.comm();
+    //iunpack.reserve(basis_.nrods() * partition.local_size());
+    iunpack.reserve(basis_.size() * partition.local_size());
 
     int isource = 0;
-    for (int iproc = 0; iproc < comm_.size(); ++iproc) {
-        for (int irod = 0; irod < basis_.nrod_loc(iproc); ++irod) {
-            int h = basis_.rod_h(iproc, irod);
-            int k = basis_.rod_k(iproc, irod);
+    for (int iproc = 0; iproc < comm.size(); ++iproc) {
+        //for (int irod = 0; irod < basis_.nrod_loc(iproc); ++irod) {
+        for (int irod = 0; irod < bm.np2_loc(iproc); irod++) {
+            //int h = basis_.rod_h(iproc, irod);
+            //int k = basis_.rod_k(iproc, irod);
+	    long global_index = bm.np2_first(iproc) + irod; 
+
+            auto coords = basis_.point_op().from_symmetric_range(basis_.to_symmetric_range(global_index));
+
+       	    int h = coords[0]; 
+      	    int k = coords[1]; 
             if (h < 0) h += np0;
             if (k < 0) k += np1;
 
@@ -156,12 +250,12 @@ std::vector<int> generate_iunpack(const block_cyclic_partition& partition, int n
     return iunpack;
 }
 
-void TDMLWFTransform::transpose_fwd(const inq::gpu::array<std::complex<double>, 1>& zvec, inq::gpu::array<std::complex<double>, 1>& ct) {
+void basis_mapping::transpose_fwd(const inq::gpu::array<std::complex<double>, 1>& zvec, inq::gpu::array<std::complex<double>, 1>& ct) {
     int np0 = basis_.sizes()[0];
     int np1 = basis_.sizes()[1];
     int np2 = basis_.sizes()[2];
     int nvec = ...; //how to get this in Inq?
-    int block_size = ...;
+    int block_size = ...; 
 
 
     block_cyclic_partition partition(np2, comm_.size(), comm_.rank(), block_size);
@@ -209,12 +303,12 @@ void TDMLWFTransform::transpose_fwd(const inq::gpu::array<std::complex<double>, 
 
 }
 
-void TDMLWFTransform::transpose_bwd(const inq::gpu::array<std::complex<double>, 1>& ct, inq::gpu::array<std::complex<double>, 1>& zvec) {
+void basis_mapping::transpose_bwd(const inq::gpu::array<std::complex<double>, 1>& ct, inq::gpu::array<std::complex<double>, 1>& zvec) {
     int np0 = basis_.sizes()[0];
     int np1 = basis_.sizes()[1];
     int np2 = basis_.sizes()[2];
-    int nvec = ...;
-    int block_size = ...;
+    int nvec = ...; 
+    int block_size = ...; 
 
 
     block_cyclic_partition partition(np2, comm_.size(), comm_.rank(), block_size);
@@ -264,7 +358,7 @@ void TDMLWFTransform::transpose_bwd(const inq::gpu::array<std::complex<double>, 
 
 //these functions require some equivalent to ip_ and im_ as implemented in Qbach's BasisMapping class
 //also, std::conj should be replaced by conj function written by Chris
-void TDMLWFTransform::vector_to_zvec(const inq::gpu::array<std::complex<double>, 1>& c, inq::gpu::array<std::complex<double>, 1>& zvec) {
+void basis_mapping::vector_to_zvec(const inq::gpu::array<std::complex<double>, 1>& c, inq::gpu::array<std::complex<double>, 1>& zvec) {
     int ng = basis_.localsize();
     int len = zvec.size();
     zvec.fill(0.0);
@@ -281,7 +375,7 @@ void TDMLWFTransform::vector_to_zvec(const inq::gpu::array<std::complex<double>,
     });
 }
 
-void TDMLWFTransform::zvec_to_vector_inq(const inq::gpu::array<std::complex<double>, 1>& zvec, inq::gpu::array<std::complex<double>, 1>& c) {
+void basis_mapping::zvec_to_vector_inq(const inq::gpu::array<std::complex<double>, 1>& zvec, inq::gpu::array<std::complex<double>, 1>& c) {
     int ng = basis_.localsize();
 
     gpu::run(ng, [&](int ig) {
@@ -294,8 +388,8 @@ void TDMLWFTransform::zvec_to_vector_inq(const inq::gpu::array<std::complex<doub
 }  // namespace inq
 
 #endif  // INQ__WANNIER__BASIS_MAPPING
-
-
+	
+	
 ///////////////////////////////////////////////////////////////////
 #ifdef INQ_WANNIER_BASIS_MAPPING_UNIT_TEST
 #undef INQ_WANNIER_BASIS_MAPPING_UNIT_TEST
@@ -306,4 +400,5 @@ TEST_CASE(INQ_TEST_FILE, INQ_TEST_TAG) {
     using namespace Catch::literals;
     using Catch::Approx;
 }
-#endif
+#endif 
+
