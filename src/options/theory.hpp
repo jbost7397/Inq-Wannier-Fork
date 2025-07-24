1c2f2c31dbd3847bfb785615875ae7fa74ef43f5
Christopher Corey Shepard <chrsshpr@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2025-07-24 21:37:40 +0000

    add hse to options::theory

--- src/options/theory.hpp
+++ src/options/theory.hpp
@@ -101,6 +101,14 @@ public:
 		return inter;
 	}
 
+	auto hse()  const {
+		theory inter = *this;
+                inter.hartree_potential_ = true;
+                inter.exchange_ = XC_HYB_GGA_XC_HSE06;
+                inter.correlation_ = XC_NONE;
+                return inter;
+        }
+		
 	auto exchange_coefficient() const {
 		if(exchange() == XC_HARTREE_FOCK) return 1.0;
 		if(exchange() == XC_NONE) return 0.0;
